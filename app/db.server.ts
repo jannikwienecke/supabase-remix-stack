6133a888c512a8308a5b25be5c6fbdec7845d9f4
Jannik Wienecke <jannik.wienecke@sirum.de>
2022-04-03 18:19:09 +0200

    change database url to supabase

--- app/db.server.ts
+++ app/db.server.ts
@@ -37,7 +37,7 @@ function getClient() {
   const isReadReplicaRegion = !PRIMARY_REGION || PRIMARY_REGION === FLY_REGION;
 
   if (!isLocalHost) {
-    databaseUrl.host = `${FLY_REGION}.${databaseUrl.host}`;
+    // databaseUrl.host = `${FLY_REGION}.${databaseUrl.host}`;
     if (!isReadReplicaRegion) {
       // 5433 is the read-replica port
       databaseUrl.port = "5433";
